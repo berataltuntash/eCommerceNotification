3f106844ff28ac01ea0fbc6500859fdf2e0723fe
berat <berataltuntas@std.iyte.edu.tr>
2023-08-29 15:42:04 +0300

    deleted unnecesarry things

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-//import registerNNPushToken from 'native-notify';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { CartProvider } from './CartContext';
@@ -16,58 +15,6 @@ import ProfilePage from './pages/ProfilePage';
 const Stack = createStackNavigator();
 
 const App = () => {
-  //registerNNPushToken(10901, 'VWZuJbRZWdU0Z6u95uUovR');
-  /*
-  const requestUserPermission = async () => {
-    const authStatus = await messaging().requestPermission();
-  const enabled =
-    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
-
-  if (enabled) {
-    console.log('Authorization status:', authStatus);
-    }
-  }
-
-  useEffect(() => {
-    if(requestUserPermission){
-      messaging().getToken().then(token => {
-        console.log(token);
-      });
-    }
-    else{
-      console.log("No permission", authStatus);
-    }
-
-    messaging()
-      .getInitialNotification()
-      .then(async (remoteMessage) => {
-        if (remoteMessage) {
-          console.log(
-            'Notification caused app to open from quit state:',
-            remoteMessage.notification,
-          );
-        }
-      });
-
-    messaging().onNotificationOpenedApp(async (remoteMessage) => {
-      console.log(
-        'Notification caused app to open from background state:',
-        remoteMessage.notification,
-      );
-    }); 
-    messaging().setBackgroundMessageHandler(async remoteMessage => {
-      console.log('Message handled in the background!', remoteMessage);
-    });
-
-    const unsubscribe = messaging().onMessage(async remoteMessage => {
-      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
-    });
-
-    return unsubscribe;
-  }, []);
-  */
-
   return (
     <CartProvider>
       <NavigationContainer>
